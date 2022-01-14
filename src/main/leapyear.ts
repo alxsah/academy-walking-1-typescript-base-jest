81f007ef445fa45996f3c2f586d132989f860e51
Alexander Sahin <alexander.sahin@cazoo.co.uk>
2022-01-14 11:20:20 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/leapyear.ts
+++ src/main/leapyear.ts
@@ -1,6 +1,12 @@
 export const isLeapYear = (year: number): boolean => {
-  if (year % 4 === 0 && year !== 1800) {
+  if (isDivisibleBy(year, 4) && isDivisibleBy(year, 100) && isDivisibleBy(year, 400)) {
+      return true;
+  } else if (isDivisibleBy(year, 4) && isDivisibleBy(year, 100)) {
+      return false;
+  } else if (isDivisibleBy(year, 4)) {
     return true;
   }
   return false;
 };
+
+const isDivisibleBy = (dividend: number, divisor: number) => dividend % divisor === 0;
--- src/test/leapyear.test.ts
+++ src/test/leapyear.test.ts
@@ -3,22 +3,12 @@ import {isLeapYear} from "../main/leapyear";
 describe ("Leap Year", () => {
   it.each([
     [1582, false],
-  ])("for 1582 should return false", (input: number, expected: boolean) => {
+    [1800, false],
+    [1996, true],
+    [2000, true],
+    [2024, true],
+    [1900, false]
+  ])("for %i should return %s", (input: number, expected: boolean) => {
     expect(isLeapYear(input)).toBe(expected);
-  }) 
-  it("For 1800 should return false", () => {
-    expect(isLeapYear(1800)).toBe(false);
-  });
-  it("For 1996 should return True", () => {
-      expect(isLeapYear(1996)).toBe(true);
-  });
-  it("For 2000 should return True", () => {
-    expect(isLeapYear(2000)).toBe(true);
-  });
-  it("For 2024 should return True", () => {
-    expect(isLeapYear(2024)).toBe(true);
-  });
-  it("For 1900 should return True", () => {
-    expect(isLeapYear(1900)).toBe(false);
-  });
+  })
 });
