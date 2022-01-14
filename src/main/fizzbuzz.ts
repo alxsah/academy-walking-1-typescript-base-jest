66b585e354eca1024f2a6b795e0caa330bb7074b
Alexander Sahin <alexander.sahin@cazoo.co.uk>
2022-01-14 10:19:58 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,7 +1,7 @@
 export const fizzbuzz = (num: number) => {
   if (num % 3 === 0) {
     return "Fizz";
-  } else if (num === 5) {
+  } else if (num % 5 === 0) {
     return "Buzz";
   }
   return num.toString();
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -5,8 +5,12 @@ describe('Fizzbuzz', () => {
     [1, "1"],
     [2, "2"],
     [3, "Fizz"],
+    [4, "4"],
     [5, "Buzz"],
     [6, "Fizz"],
+    [7, "7"],
+    [8, "8"],
+    [10, "Buzz"]
   ])("takes %i and returns %s", (input: number, expected: string) => {
     expect(fizzbuzz(input)).toBe(expected);
   });
