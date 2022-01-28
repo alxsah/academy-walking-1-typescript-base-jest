6027905bdd0b733c201d3009de3068fcc8318f09
Josh Emery <josh.emery@smartfleet.co.uk>
2022-01-28 11:17:01 +0000

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/test/romanNumberConvertor.test.ts
@@ -0,0 +1,9 @@
+describe('convert an integer to roman number', () => {
+    it("an integer of 1 returns 'I'", () => {
+        expect(romanNumberConvertor(1)).toBe('I');
+    })
+})
+
+function romanNumberConvertor(number: number) {
+    return undefined;
+}
