4c5349c037ac4c5a3e456f758241f86513bc3a69
Alexander Sahin <alexander.sahin@cazoo.co.uk>
2022-01-28 11:20:08 +0000

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/main/romanNumberConvertor.ts
@@ -0,0 +1,3 @@
+export function romanNumberConvertor(number: number): string {
+    return 'I';
+}
--- src/test/romanNumberConvertor.test.ts
+++ src/test/romanNumberConvertor.test.ts
@@ -1,9 +1,11 @@
+import { romanNumberConvertor } from "../main/romanNumberConvertor";
+
 describe('convert an integer to roman number', () => {
     it("an integer of 1 returns 'I'", () => {
         expect(romanNumberConvertor(1)).toBe('I');
     })
+    it("an integer of 2 returns 'II'", () => {
+      expect(romanNumberConvertor(2)).toBe('II');
+    })
 })
 
-function romanNumberConvertor(number: number) {
-    return undefined;
-}
