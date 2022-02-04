df5ac557e47687cd5b0a3c198f40811a2836b662
Alexander Sahin <alexander.sahin@cazoo.co.uk>
2022-02-04 11:35:35 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/ticTacToe/board.ts
+++ src/main/ticTacToe/board.ts
@@ -8,15 +8,13 @@ export class Board {
 
   playMove(number: number) {
     if (this.state[number] === 'x' || this.state[number] === 'o')
-    throw new Error('Move not allowed');
+      throw new Error('Move not allowed');
     if (this.move % 2 === 0) {
       this.state[number] = 'x';
     } else {
       this.state[number] = 'o';
     }
-
     this.move++;
-
     return this;
   }
 }
--- src/main/ticTacToe/ticTacToe.ts
+++ src/main/ticTacToe/ticTacToe.ts
@@ -9,33 +9,32 @@ export class TicTacToeGame {
 
   hasWon() {
     const state = this.board.getState();
-    let hasPlayerWon = false;
+
+    if (hasWonDiagonally(state)) return true;
     for (let i=0; i<3; i++) {
       if (hasWonVertically(state, i) || hasWonHorizontally(state, i)) return true;
     }
-    if (hasWonDiagonally(state)) return true;
     return false;
   }
 }
 
-const hasWonVertically = (state: string[], startPosition: number) => {
-  const positionsPlayed = state[startPosition] === 'x' || state[startPosition] === 'o';
-  return state[startPosition] === state[startPosition + 3] && state[startPosition] === state[startPosition + 6];
-}
+const hasWonVertically = (state: string[], startPosition: number) =>
+  positionHasBeenPlayed(state[startPosition]) && (state[startPosition] === state[startPosition + 3] && state[startPosition] === state[startPosition + 6]);
 
-const hasWonHorizontally = (state: string[], startPosition: number) => {
-  const positionsPlayed = state[startPosition] === 'x' || state[startPosition] === 'o';
-  return state[startPosition] === state[startPosition + 1] && state[startPosition] === state[startPosition + 2];
-}
+const hasWonHorizontally = (state: string[], startPosition: number) =>
+  positionHasBeenPlayed(state[startPosition]) && state[startPosition] === state[startPosition + 1] && state[startPosition] === state[startPosition + 2];
+
 
 const hasWonDiagonally = (state: string[]) => {
   const winStates = [[0, 4, 8], [2, 4, 6]];
-  for (let winState of winStates) {
-    if (state[winState[0]] === state[winState[1]] && state[winState[0]] === state[winState[2]] && positionHasBeenPlayed(state[winState[0]])) {
-      return true;
+  let result = false;
+
+  winStates.forEach((winState: number[]) => {
+    if (positionHasBeenPlayed(state[winState[0]]) && state[winState[0]] === state[winState[1]] && state[winState[0]] === state[winState[2]]) {
+      result = true;
     }
-  }
-  return false;
+  });
+  return result;
 }
 
 const positionHasBeenPlayed = (position: string) => position === 'x' || position === 'o';
