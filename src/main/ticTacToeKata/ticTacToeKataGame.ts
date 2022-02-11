2b6ab9eee41aaf1d0bac3f2952585ffe92c35173
Alexander Sahin <alexander.sahin@cazoo.co.uk>
2022-02-11 10:36:48 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/ticTacToeKata/ticTacToeKataGame.ts
+++ src/main/ticTacToeKata/ticTacToeKataGame.ts
@@ -1,7 +1,9 @@
+import { Player } from './Player';
+
 export class TicTacToeKataGame {
 
   private ticTacToeBoard: string[][] = [[], [], []];
-  private currentPlayer: string = "X";
+  private currentPlayer: string = Player.X;
   constructor() {
     this.createBoard();
   }
@@ -9,21 +11,29 @@ export class TicTacToeKataGame {
   createBoard()  {
     for(let row = 0 ; row < 3; row++){
       for(let col = 0 ; col < 3; col++){
-        this.ticTacToeBoard[row][col] = "-";
+        this.ticTacToeBoard[row][col] = Player.EMPTY;
       }
     }
   }
 
   getNextPlayer(){
-   if (this.currentPlayer === "X") return "O";
-   return "X";
+   if (this.currentPlayer === Player.X) return Player.O;
+   return Player.X;
   }
+
   play(row: number, col: number) {
+    if (this.getBoardSymbolAt(row, col) !== Player.EMPTY) {
+      throw new Error("Cannot play on already played position");
+    }
     this.ticTacToeBoard[row][col] = this.currentPlayer;
     this.currentPlayer = this.getNextPlayer();
   }
 
   getBoardSymbolAt(row: number, col: number) {
     return this.ticTacToeBoard[row][col];
+  }
+
+  winner() {
+    
   }
 }
--- src/test/ticTacToeKata.test.ts
+++ src/test/ticTacToeKata.test.ts
@@ -1,6 +1,5 @@
 import {TicTacToeKataGame} from "../main/ticTacToeKata/ticTacToeKataGame";
 
-
 describe("tic tac toe game", () => {
 
   it("Player X goes first always", () => {
@@ -15,6 +14,7 @@ describe("tic tac toe game", () => {
     ticTacToe.play(1,2);
     expect(ticTacToe.getBoardSymbolAt(1,2)).toBe("O");
   })
+
   it("Player X goes next", () => {
     const ticTacToe = new TicTacToeKataGame();
     ticTacToe.play(1,1);
@@ -22,10 +22,23 @@ describe("tic tac toe game", () => {
     ticTacToe.play(2,2);
     expect(ticTacToe.getBoardSymbolAt(2,2)).toBe("X");
   })
+
   it("Player cannot play on played position", () => {
     const ticTacToe = new TicTacToeKataGame();
     ticTacToe.play(1,1);
-    ticTacToe.play(1,1);
-    expect(ticTacToe.getBoardSymbolAt(1,1)).toBe("X");
-  }) 
+    expect(() => {
+      ticTacToe.play(1,1)
+    }).toThrowError("Cannot play on already played position");
+  })
+
+  it("Player wins when they have 3 symbols in a row horizontally", () => {
+    const ticTacToe = new TicTacToeKataGame();
+    ticTacToe.play(0,0);
+    ticTacToe.play(1,0);
+    ticTacToe.play(0,1);
+    ticTacToe.play(1, 1);
+    ticTacToe.play(0,2);
+    expect(ticTacToe.winner()).toBe("X");
+  })
+
 })
